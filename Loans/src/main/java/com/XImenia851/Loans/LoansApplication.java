8e3f86890eb4388f01fd8a96ec8e1164b97b70d2
Ximenia Hagard <ximenia.hagardlaplateforme.io>
2026-04-16 11:00:15 +0200

    feat : now making the last microservice Loans. Cards finished.

---
+++ Loans/src/main/java/com/XImenia851/Loans/LoansApplication.java
@@ -0,0 +1,13 @@
+package com.XImenia851.Loans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
